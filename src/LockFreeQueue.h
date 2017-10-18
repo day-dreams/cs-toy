d1f069d72d46b72260391f0208e76deb90ea4121
daydream <zn.moon2016@gmail.com>
2017-10-18 19:42:59 +0800

    set max size of queue

--- src/LockFreeQueue.h
+++ src/LockFreeQueue.h
@@ -14,19 +14,33 @@ using std::mutex;
 template <typename T> class LockFreeQueue {
 private:
   queue<T> _queue;
-  mutex lock;
+  mutex lock, full;
   sem_t client_arrived;
+  int maxnum;
 
 public:
-  LockFreeQueue() { sem_init(&client_arrived, 0, 0); }
+  LockFreeQueue() {
+    sem_init(&client_arrived, 0, 0);
+    maxnum = 30;
+  }
+  LockFreeQueue(int maxnum) {
+    sem_init(&client_arrived, 0, 0);
+    this->maxnum = maxnum;
+  }
 
   /*
       push 向队列中加入一个元素
   */
   void push(const T &ele) {
 
+    full.lock(); // if queue is full,this will block until pop() unlcok it
+
     lock.lock();
     _queue.push(ele);
+    if (_queue.size() == maxnum) { // queue is full,so hold full mutex
+    } else {                       // or,just unlock it
+      full.unlock();
+    }
     lock.unlock();
 
     sem_post(&client_arrived);
@@ -39,8 +53,12 @@ public:
     sem_wait(&client_arrived);
 
     lock.lock();
+
+    if (_queue.size() == maxnum) // queue is not full anymore,so unlock it
+      full.unlock();
     auto result = _queue.front();
     _queue.pop();
+
     lock.unlock();
 
     return result;
